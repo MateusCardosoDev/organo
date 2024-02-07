9782298203931439dd97f8306f4fc2aaccbe2788
mateuscardosodev <contato.mateuscardoso@outlook.com>
2024-02-07 17:28:41 -0300

    Criando e costumizando os inputs de texto

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import Banner from './components/Banner/Banner.jsx'
+import Banner from './components/Banner/index.jsx'
+import CampoTexto from './components/index.jsx'
 
 function App() {
 
   return (
     <div className='App'>
       <Banner/>
+      <CampoTexto label="Nome" placeholder="Digite seu nome"/>
+      <CampoTexto label="Cargo" placeholder="Digite seu cargo"/>
+      <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
     </div>
   )
 }
