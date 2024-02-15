8a71967a53d4f848c8107dafab1504962f514248
mateuscardosodev <contato.mateuscardoso@outlook.com>
2024-02-15 17:06:58 -0300

    mapeando os colaboradores

--- src/components/Colaborador/index.jsx
+++ src/components/Colaborador/index.jsx
@@ -1,17 +1,14 @@
 import "./Colaborador.css";
 
-const Colaborador = () => {
+const Colaborador = ({ nome, cargo, imagem }) => {
   return (
     <div className="colaborador">
       <div className="cabecalho">
-        <img
-          src="https://github.com/mateuscardosodev.png"
-          alt="Mateus Cardoso"
-        />
+        <img src={imagem} alt={nome} />
       </div>
       <div className="rodape">
-        <h4>Mateus Cardoso</h4>
-        <h5>Front-End</h5>
+        <h4>{nome}</h4>
+        <h5>{cargo}</h5>
       </div>
     </div>
   );
