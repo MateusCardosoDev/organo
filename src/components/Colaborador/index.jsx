2bf913dd8e684d9e538f2342d1b6021c0cd72f1c
mateuscardosodev <contato.mateuscardoso@outlook.com>
2024-02-15 16:32:03 -0300

    card de colaborador

---
+++ src/components/Colaborador/index.jsx
@@ -0,0 +1,20 @@
+import "./Colaborador.css";
+
+const Colaborador = () => {
+  return (
+    <div className="colaborador">
+      <div className="cabecalho">
+        <img
+          src="https://github.com/mateuscardosodev.png"
+          alt="Mateus Cardoso"
+        />
+      </div>
+      <div className="rodape">
+        <h4>Mateus Cardoso</h4>
+        <h5>Front-End</h5>
+      </div>
+    </div>
+  );
+};
+
+export default Colaborador;
